954ad5fcc7f47e3293ca37667f710f32542ceb5d
rswastaken <h1910153@nushigh.edu.sg>
2021-12-09 21:30:34 +0800

    PptUtils.java now searches for images, text in comments and in slides

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,7 @@
 package application;
 
 import application.Model.PptUtils;
+import application.Model.Types.PptData;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,13 +9,18 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         FXMLLoader stageLoader = new FXMLLoader(getClass().getResource("View/splashscreen.fxml"));
-        PptUtils.decode(new File("data/ppt/sample1.pptx"));
+        PptData data = PptUtils.decode(new File("data/ppt/sample1.pptx"));
+        ArrayList<String> slideTxt = data.getSlideTxt();
+        ArrayList<String> commentTxt = data.getCommentTxt();
+        System.out.println(slideTxt.toString());
+        System.out.println(commentTxt.toString());
         Parent root = stageLoader.load();
         primaryStage.setTitle("Hackathon Submission");
         primaryStage.setScene(new Scene(root, 300, 275));
--- src/application/Model/PptUtils.java
+++ src/application/Model/PptUtils.java
@@ -2,12 +2,19 @@ package application.Model;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.awt.image.BufferedImage;
+
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
+
+import application.Model.Types.PptData;
+import javafx.scene.image.Image;
 import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
+import javax.imageio.ImageIO;
 import javax.xml.parsers.*;
 
 
@@ -15,11 +22,17 @@ public class PptUtils {
     public PptUtils() {
 
     }
-    public static void decode(File file) throws IOException, ParserConfigurationException, SAXException {
+
+    public static PptData decode(File file) throws IOException, ParserConfigurationException, SAXException {
         ZipFile zFile = new ZipFile(file);
         Enumeration<? extends ZipEntry> entries = zFile.entries();
         File contentType = new File("data/ppt/sample1/[Content_Types].xml");
         OutputStream zOutStream;
+        ArrayList<String> slideTxt = new ArrayList<String>();
+        ArrayList<String> commentTxt = new ArrayList<String>();
+        ArrayList<BufferedImage> images = new ArrayList<BufferedImage>();
+        int slideCount = 0;
+        int commentCount = 0;
         while (entries.hasMoreElements()) {
             ZipEntry zEntry = entries.nextElement();
             String[] zPath = zEntry.getName().split("/");
@@ -27,28 +40,54 @@ public class PptUtils {
             Element root;
             if (zEntry.isDirectory()) {
                 System.out.println("---------------------Directory: " + zEntry.getName());
+            } else if (zPath[0].equals("ppt") && zPath[1].equals("media")) {
+                String ext = zPath[2].split("\\.")[1];
+                if (ext.matches("jpg|gif|png|bmp|tif|webp")) {
+                    System.out.println("&&&&&&" + ext);
+                    BufferedImage zImage = getImage(zFile, zEntry);
+                    images.add(zImage);
+                    ImageIO.write(zImage, ext, new File("data/ppt/sample1/ppt/media/new" + zPath[2]));
+                }
             } else if (zPath[0].equals("ppt") && zPath[1].equals("slides")) {
-                zDoc = getDocument(zFile,zEntry);
-                root = zDoc.getDocumentElement();
-                NodeList a_t = root.getElementsByTagName("a:t");
-                for (int i=0;i<a_t.getLength();i++) {
-                    System.out.println(a_t.item(i).getTextContent());
+                if (!zPath[2].equals("_rels")) {
+                    slideCount += 1;
+                    slideTxt.add("Slide " + slideCount);
+                    zDoc = getDocument(zFile, zEntry);
+                    root = zDoc.getDocumentElement();
+                    NodeList a_t = root.getElementsByTagName("a:t");
+                    for (int i = 0; i < a_t.getLength(); i++) {
+                        slideTxt.add(a_t.item(i).getTextContent());
+                    }
+                }
+            } else if (zPath[0].equals("ppt") && zPath[1].equals("notesSlides")) {
+                if (!zPath[2].equals("_rels")) {
+                    commentCount += 1;
+                    commentTxt.add("Slide " + commentCount);
+                    zDoc = getDocument(zFile, zEntry);
+                    root = zDoc.getDocumentElement();
+                    NodeList a_t = root.getElementsByTagName("a:t");
+                    for (int i = 0; i < a_t.getLength(); i++) {
+                        commentTxt.add(a_t.item(i).getTextContent());
+                    }
                 }
 
             } else if (zEntry.getName().equals("[Content_Types].xml")) {
-                zDoc = getDocument(zFile,zEntry);
+                zDoc = getDocument(zFile, zEntry);
                 root = zDoc.getDocumentElement();
                 NodeList defaultNodes = root.getElementsByTagName("Default");
-                for (int i=0;i<defaultNodes.getLength();i++) {
-                    System.out.println((((Element)(defaultNodes.item(i))).getAttribute("ContentType")));
+                for (int i = 0; i < defaultNodes.getLength(); i++) {
+                    System.out.println((((Element) (defaultNodes.item(i))).getAttribute("ContentType")));
                 }
                 System.out.println("---------------------Found ContentType XML");
             } else {
                 System.out.println(zEntry.getName());
             }
         }
+        PptData data = new PptData(slideTxt, commentTxt);
         zFile.close();
+        return data;
     }
+
     private static Document getDocument(ZipFile file, ZipEntry entry) throws IOException, ParserConfigurationException, SAXException {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
@@ -58,5 +97,15 @@ public class PptUtils {
         ByteArrayInputStream zInputStream = new ByteArrayInputStream(zBuffer);
         zStream.close();
         return builder.parse(zInputStream);
+    }
+
+    private static BufferedImage getImage(ZipFile file, ZipEntry entry) throws IOException {
+        InputStream zStream;
+        zStream = file.getInputStream(entry);
+        byte[] zBuffer = zStream.readAllBytes();
+        ByteArrayInputStream zInputStream = new ByteArrayInputStream(zBuffer);
+        BufferedImage zImage = ImageIO.read(zInputStream);
+        zStream.close();
+        return zImage;
     }
 }
