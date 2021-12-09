729fb1a8d6e0e244c6472dca87065892288020d2
rswastaken <h1910153@nushigh.edu.sg>
2021-12-09 18:22:58 +0800

    merge changes

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,6 @@
 package application;
 
+import application.Model.PptUtils;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -13,8 +14,9 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         FXMLLoader stageLoader = new FXMLLoader(getClass().getResource("View/splashscreen.fxml"));
+        PptUtils.decode(new File("data/ppt/sample1.pptx"));
         Parent root = stageLoader.load();
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Hackathon Submission");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
