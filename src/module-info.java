358a4a9075dc4158c763b3122a6e5e7a583751d8
rswastaken <h1910153@nushigh.edu.sg>
2021-12-09 17:30:34 +0800

    test extracting text nodes from slides

--- src/module-info.java
+++ src/module-info.java
@@ -3,6 +3,7 @@ module DecHackathon {
     requires javafx.controls;
     requires javafx.base;
     requires javafx.media;
+    requires java.xml;
     opens application.Controller;
     opens application.View;
     opens application;
